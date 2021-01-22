cb7cf7b03d12d9a36899c3b67358e3f2f19cdc62
Robin Delbaere <hello@rdelbaere.fr>
2021-01-22 15:57:37 +0100

    Correction : service

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -14,10 +14,7 @@ class MainController extends AbstractController
      */
     public function home(): Response
     {
-
-        return $this->render('main/home.html.twig', array(
-
-        ));
+        return $this->render('main/home.html.twig');
     }
 
 }
